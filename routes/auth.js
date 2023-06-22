bd082d17d0b04de8bccbc71f7705efcee26133b5
DEVANAND R <83712043+devanandr001@users.noreply.github.com>
2023-06-22 15:49:39 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { verify } = require('jsonwebtoken');
 const router = express.Router();
 
-// const handleLogin = require('../controllers/loginController');
+const handleLogin = require('../controllers/loginController');
 const handleLogout = require('../controllers/logoutController');
 // const refreshToken = require('../controllers/refreshTokenController');
 // const resetPassword = require('../controllers/passwordResetController');
 // const verifyMail = require('../controllers/emailVerification');
 
-// router.get('/login', handleLogin);
-// router.post('/login', handleLogin);
+router.get('/login', handleLogin);
+router.post('/login', handleLogin);
 router.get('/logout', handleLogout);
 // router.get('/refresh', refreshToken);
 // router.get('/register', registerUser);
