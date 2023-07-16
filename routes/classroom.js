d373b1128c09b89b08f3761a89ac86f422e71bc7
Tanmay Jajotar <tanmayjajotar@gmail.com>
2023-07-16 17:44:53 +0530

    classroom API added

---
+++ routes/classroom.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const classroomController = require('../controllers/classroomController.js');
+const checkRole = require('../middleware/checkRoleMiddleware.js');
+
+// Create a classroom
+router.post('/create', checkRole(['admin','teacher']), classroomController.createClassroom);
+
+// Add a student to a classroom
+router.post('/addStudent',checkRole(['admin','teacher']), classroomController.addStudentToClassroom);
+
+module.exports = router;
